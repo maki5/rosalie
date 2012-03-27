18bb0224e82f740ff11f37dde4eb9527aea61afd
Alexandr Ababii <work.maki.5@gmail.com>
2012-03-27 18:15:42 +0300

    initial commit

---
+++ lib/rosalie.rb
@@ -0,0 +1,2 @@
+module Rosalie
+end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'c3202916d013a85629d4725cb0b4dcd1f35b55a0f30b5254062687ef803099990a5e3839d1c64907290edaf506672c33a6229a5559114304e00fd23a3f760685'
