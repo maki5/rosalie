c3bb6752fcde9cab847e18ee1621d87107d6824d
Alexandr Ababii <work.maki.5@gmail.com>
2012-03-29 00:08:51 +0300

    Revert "modified version changing"
    This reverts commit 89d1a523715adf3ff9e3a8dece77f5d72c4fec96.

--- lib/rosalie/version.rb
+++ lib/rosalie/version.rb
@@ -1,8 +1,3 @@
 module Rosalie
-  module Version
-    Described = (IO::popen('git describe --long') { |gs| gs.read }).strip
-    Number = Described.gsub('-', '.').gsub(/^v/, '').split('.')[0..-2].join('.')
-    Date = Date.strptime( IO::popen('git show -s --format="%ct"') { |gs| gs.read } , "%s")
-    Revision = (IO::popen('git show -s --format="%H"') { |gs| gs.read }).strip
-  end
+  VERSION = "0.0.1"
 end
