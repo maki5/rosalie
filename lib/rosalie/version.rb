89d1a523715adf3ff9e3a8dece77f5d72c4fec96
Alexandr Ababii <work.maki.5@gmail.com>
2012-03-29 00:06:42 +0300

    modified version changing

--- lib/rosalie/version.rb
+++ lib/rosalie/version.rb
@@ -1,3 +1,8 @@
 module Rosalie
-  VERSION = "0.0.1"
+  module Version
+    Described = (IO::popen('git describe --long') { |gs| gs.read }).strip
+    Number = Described.gsub('-', '.').gsub(/^v/, '').split('.')[0..-2].join('.')
+    Date = Date.strptime( IO::popen('git show -s --format="%ct"') { |gs| gs.read } , "%s")
+    Revision = (IO::popen('git show -s --format="%H"') { |gs| gs.read }).strip
+  end
 end
